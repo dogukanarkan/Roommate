f40c64e57a55f120e1a54480b0e57818fe346396
Doğukan Arkan <mail@dogukanarkan.com>
2018-04-18 19:30:07 +0300

    Proje içerikleri oluşturuldu.

---
+++ app/src/main/java/com/example/lykia/roommate/DetailArticleActivity.java
@@ -0,0 +1,13 @@
+package com.example.lykia.roommate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailArticleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_article);
+    }
+}
