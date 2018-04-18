8fa9c7caa5e0c814374eb5fe6aa7adeea2d04de3
Doğukan Arkan <mail@dogukanarkan.com>
2018-04-19 00:46:24 +0300

    DTO class'ları tamamlandı.

--- app/src/main/java/com/example/lykia/roommate/DTOs/AdminDTO.java
+++ app/src/main/java/com/example/lykia/roommate/DTOs/AdminDTO.java
@@ -1,4 +1,70 @@
 package com.example.lykia.roommate.DTOs;
 
+import java.sql.Date;
+
 public class AdminDTO {
+
+    private int account_id;
+    private String username;
+    private String password;
+    private String first_name;
+    private String last_name;
+    private Date register_date;
+    private Date last_login;
+
+    public int getAccount_id() {
+        return account_id;
+    }
+
+    public void setAccount_id(int account_id) {
+        this.account_id = account_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public Date getRegister_date() {
+        return register_date;
+    }
+
+    public void setRegister_date(Date register_date) {
+        this.register_date = register_date;
+    }
+
+    public Date getLast_login() {
+        return last_login;
+    }
+
+    public void setLast_login(Date last_login) {
+        this.last_login = last_login;
+    }
 }
--- app/src/main/java/com/example/lykia/roommate/DTOs/AnimalDTO.java
+++ app/src/main/java/com/example/lykia/roommate/DTOs/AnimalDTO.java
@@ -1,4 +1,23 @@
 package com.example.lykia.roommate.DTOs;
 
 public class AnimalDTO {
+
+    private int Animal;
+    private String animal_name;
+
+    public int getAnimal() {
+        return Animal;
+    }
+
+    public void setAnimal(int animal) {
+        Animal = animal;
+    }
+
+    public String getAnimal_name() {
+        return animal_name;
+    }
+
+    public void setAnimal_name(String animal_name) {
+        this.animal_name = animal_name;
+    }
 }
