e29827738d43af3c2c5dd24a1662ee232bfe6582
Doğukan Arkan <mail@dogukanarkan.com>
2018-04-19 00:46:24 +0300

    DTO class'ları tamamlandı.

--- app/src/main/java/com/example/lykia/roommate/DTOs/RaceDTO.java
+++ app/src/main/java/com/example/lykia/roommate/DTOs/RaceDTO.java
@@ -1,4 +1,32 @@
 package com.example.lykia.roommate.DTOs;
 
 public class RaceDTO {
+
+    private int race_id;
+    private AnimalDTO animal_id;
+    private String race_name;
+
+    public int getRace_id() {
+        return race_id;
+    }
+
+    public void setRace_id(int race_id) {
+        this.race_id = race_id;
+    }
+
+    public AnimalDTO getAnimal_id() {
+        return animal_id;
+    }
+
+    public void setAnimal_id(AnimalDTO animal_id) {
+        this.animal_id = animal_id;
+    }
+
+    public String getRace_name() {
+        return race_name;
+    }
+
+    public void setRace_name(String race_name) {
+        this.race_name = race_name;
+    }
 }
--- app/src/main/java/com/example/lykia/roommate/DTOs/UserDTO.java
+++ app/src/main/java/com/example/lykia/roommate/DTOs/UserDTO.java
@@ -1,4 +1,79 @@
 package com.example.lykia.roommate.DTOs;
 
+import java.sql.Date;
+
 public class UserDTO {
+
+    private int user_id;
+    private String mail;
+    private String image_path;
+    private String first_name;
+    private String last_name;
+    private String location;
+    private String password;
+    private Date register_date;
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getImage_path() {
+        return image_path;
+    }
+
+    public void setImage_path(String image_path) {
+        this.image_path = image_path;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getRegister_date() {
+        return register_date;
+    }
+
+    public void setRegister_date(Date register_date) {
+        this.register_date = register_date;
+    }
 }
