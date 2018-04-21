4fff55878ba129aa28741144276a0bea26038291
Tuğçe Yıldırım <tugceyildirim017@hotmail.com>
2018-04-21 17:40:00 +0300

    Mecburi commit

--- app/src/main/java/com/example/lykia/roommate/HomeActivity.java
+++ app/src/main/java/com/example/lykia/roommate/HomeActivity.java
@@ -2,12 +2,19 @@ package com.example.lykia.roommate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        listView=(ListView)findViewById(R.id.listView);
+        ArrayAdapter adapter = new ArrayAdapter()
     }
 }
