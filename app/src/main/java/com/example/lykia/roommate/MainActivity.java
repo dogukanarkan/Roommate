2772e009f6fa3b4f86dc6b0e23795d87d921b274
Hilal Erisev <herisev15@posta.pau.edu.tr>
2018-04-25 00:19:58 +0300

    Search Activity düzenlendi.

--- app/src/main/java/com/example/lykia/roommate/MainActivity.java
+++ app/src/main/java/com/example/lykia/roommate/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.example.lykia.roommate.R;
 
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     Button buttonRegister;
     Button buttonLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
