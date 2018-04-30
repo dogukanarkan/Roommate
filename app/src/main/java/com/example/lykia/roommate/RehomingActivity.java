45b2d3e9a77c7f2fdf8d29479edc34d9ce0747d1
busraakguller <busrakgllr@hotmail.com>
2018-04-30 20:25:42 +0300

    Image cropper Add Article ve Rehoming activity'lerine eklendi.

--- app/src/main/java/com/example/lykia/roommate/RehomingActivity.java
+++ app/src/main/java/com/example/lykia/roommate/RehomingActivity.java
@@ -1,5 +1,8 @@
 package com.example.lykia.roommate;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,9 +12,28 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.nio.channels.GatheringByteChannel;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class RehomingActivity extends AppCompatActivity {
 
+    private DatabaseReference mUserDatabase;
+    private FirebaseUser mCurrentUser;
+    private CircleImageView mDisplayImage;
+
     private Button mImageBtn;
+    private static  final int GALLERY_PICK=1;
+
 
     Spinner spinner;
     Spinner spinner2;
@@ -19,10 +41,14 @@ public class RehomingActivity extends AppCompatActivity {
     ArrayAdapter<CharSequence>adapter;
     ArrayAdapter<CharSequence>adapter2;
     ArrayAdapter<CharSequence>adapter3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rehoming);
+
+        mImageBtn=(Button)findViewById(R.id.addPhotoBtn);
+        mDisplayImage=(CircleImageView)findViewById(R.id.userSingleImage);
         spinner=(Spinner)findViewById(R.id.spinner);
         spinner2=(Spinner)findViewById(R.id.spinner2);
         spinner3=(Spinner)findViewById(R.id.spinner3);
@@ -46,5 +72,54 @@ public class RehomingActivity extends AppCompatActivity {
 
             }
         });
+//        mUserDatabase.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                final String image = dataSnapshot.child("image").getValue().toString();
+//                String thumb_image = dataSnapshot.child("thumb_image").getValue().toString();
+//
+//            }
+//
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        });
+
+        mImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent=new Intent();
+                galleryIntent.setType("image/*");
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+
+//                CropImage.activity()
+//                        .setGuidelines(CropImageView.Guidelines.ON)
+//                        .start(RehomingActivity.this);
+
+
+                startActivityForResult(Intent.createChooser(galleryIntent,"SELECT IMAGE"),GALLERY_PICK);
+            }
+        });
+    }
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+
+        if(requestCode==GALLERY_PICK&&resultCode==RESULT_OK){
+            Uri imageUri=data.getData();
+            CropImage.activity(imageUri)
+                    .setAspectRatio(1,1)
+                    .start(this);
+        }
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
     }
 }
+
