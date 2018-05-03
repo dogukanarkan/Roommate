902c373a7ff7f8f35d16c8a18db15bc6f333afd8
Hilal Erisev <herisev15@posta.pau.edu.tr>
2018-05-03 19:32:59 +0300

    SearchActivity tamamlandı.

--- app/src/main/java/com/example/lykia/roommate/UserProfileActivity.java
+++ app/src/main/java/com/example/lykia/roommate/UserProfileActivity.java
@@ -5,20 +5,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class UserProfileActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private TextView test;
+    private String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
+        test = (TextView) findViewById(R.id.textViewName);
         toolbar = (Toolbar) findViewById(R.id.userProfileAppBar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        userId = getIntent().getStringExtra("userId");
+        test.setText(userId);
+
     }
 
     @Override
