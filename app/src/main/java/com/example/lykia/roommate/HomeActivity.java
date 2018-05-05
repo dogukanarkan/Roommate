29ed47818368341d36e2ffef61dbb7f80a2fc0f0
Hilal Erisev <herisev15@posta.pau.edu.tr>
2018-05-05 20:49:42 +0300

    Bottom bar gerekli sayfalara eklendi.

--- app/src/main/java/com/example/lykia/roommate/HomeActivity.java
+++ app/src/main/java/com/example/lykia/roommate/HomeActivity.java
@@ -1,8 +1,11 @@
 package com.example.lykia.roommate;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -12,21 +15,48 @@ import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
-    Spinner spinnerHayvan;
-    Spinner spinnerCins;
-    Spinner spinnerCinsiyet;
-    ListView listViewAnimal;
+    private Spinner spinnerHayvan;
+    private Spinner spinnerCins;
+    private Spinner spinnerCinsiyet;
+    private ListView listViewAnimal;
+    private BottomNavigationView navButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        navButton=(BottomNavigationView)findViewById(R.id.bottomBar);
+        navButton.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.home_nav:
+                        Intent intent=new Intent(HomeActivity.this,HomeActivity.class);
+                        startActivity(intent);
+                        finish();
+                        break;
+                    case R.id.search_nav:
+                        Intent intent1=new Intent(HomeActivity.this,SearchActivity.class);
+                        startActivity(intent1);
+                        finish();
+                        break;
+                    case R.id.profile_nav:
+                        Intent intent2=new Intent(HomeActivity.this,OwnProfileActivity.class);
+                        startActivity(intent2);
+                        finish();
+                        break;
+                }
+                return false;
+            }
+        });
         spinnerHayvan=(Spinner)findViewById((R.id.animalSpinner));
         spinnerCins=(Spinner)findViewById((R.id.raceSpinner));
         spinnerCinsiyet=(Spinner)findViewById((R.id.genderSpinner));
         listViewAnimal=(ListView)findViewById(R.id.LVshowAnimal);
 
-        ArrayAdapter adapterHayvan = ArrayAdapter.createFromResource(this,R.array.hayvan, android.R.layout.simple_spinner_item);
+        ArrayAdapter adapterHayvan = ArrayAdapter.createFromResource(this,R.array.cinsiyet, android.R.layout.simple_spinner_item);
         adapterHayvan.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerHayvan.setAdapter(adapterHayvan);
         spinnerHayvan.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -42,7 +72,7 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
-        ArrayAdapter adapterCins = ArrayAdapter.createFromResource(this,R.array.tür, android.R.layout.simple_spinner_item);
+        ArrayAdapter adapterCins = ArrayAdapter.createFromResource(this,R.array.cinsiyet, android.R.layout.simple_spinner_item);
         adapterCins.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerCins.setAdapter(adapterCins);
         spinnerHayvan.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -63,7 +93,7 @@ public class HomeActivity extends AppCompatActivity {
         spinnerHayvan.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getBaseContext(), parent.getItemIdAtPosition(position)+"seçilen ", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(getBaseContext(), parent.getItemIdAtPosition(position)+"seçilen ", Toast.LENGTH_SHORT).show();
             }
 
             @Override
--- app/src/main/java/com/example/lykia/roommate/OwnProfileActivity.java
+++ app/src/main/java/com/example/lykia/roommate/OwnProfileActivity.java
@@ -1,13 +1,41 @@
 package com.example.lykia.roommate;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class OwnProfileActivity extends AppCompatActivity {
+    private BottomNavigationView navButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_own_profile);
+        navButton=(BottomNavigationView)findViewById(R.id.bottomBar);
+        navButton.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.home_nav:
+                        Intent intent=new Intent(OwnProfileActivity.this,HomeActivity.class);
+                        startActivity(intent);
+                        return true;
+                    case R.id.search_nav:
+                        Intent intent1=new Intent(OwnProfileActivity.this,SearchActivity.class);
+                        startActivity(intent1);
+                        return true;
+                    case R.id.profile_nav:
+                        Intent intent2=new Intent(OwnProfileActivity.this,OwnProfileActivity.class);
+                        startActivity(intent2);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
